47157cfc5e1afa0ed7a1eeef7909f208a8e9cdb7
info@appsahuevo.com <info@appsahuevo.com>
2022-03-22 17:58:56 -0500

    platforms commit

--- platforms/browser/www/cordova_plugins.js
+++ platforms/browser/www/cordova_plugins.js
@@ -7,31 +7,12 @@ module.exports = [
         "clobbers": [
             "cordova.plugins.autoStart"
         ]
-    },
-    {
-        "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
-        "id": "cordova-plugin-ionic-webview.IonicWebView",
-        "pluginId": "cordova-plugin-ionic-webview",
-        "clobbers": [
-            "Ionic.WebView"
-        ]
-    },
-    {
-        "file": "plugins/onesignal-cordova-plugin/www/OneSignal.js",
-        "id": "onesignal-cordova-plugin.OneSignal",
-        "pluginId": "onesignal-cordova-plugin",
-        "clobbers": [
-            "OneSignal"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-autostart": "2.3.0",
-    "cordova-plugin-ionic-webview": "4.2.1",
-    "onesignal-cordova-plugin": "2.11.4",
-    "cordova-plugin-ionic-keyboard": "2.2.0"
+    "cordova-plugin-autostart": "2.3.0"
 }
 // BOTTOM OF METADATA
 });
