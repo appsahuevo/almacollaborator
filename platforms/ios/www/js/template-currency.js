301cdc040118c0bda64743b4871ad37586505f18
info@appsahuevo.com <info@appsahuevo.com>
2022-03-22 18:18:49 -0500

    plugins commit

--- platforms/ios/www/js/template-currency.js
+++
@@ -1,4 +0,0 @@
-Template7.registerHelper('currency', function (data) {
-    var ret = '$ ' + parseInt(data).toLocaleString("es-CO");
-    return ret;
-});
--- platforms/ios/www/js/template-substr.js
+++
@@ -1,7 +0,0 @@
-Template7.registerHelper('substr', function (data) {
-    if(data && data.length > 15){
-       return data.substring(0, 15) + '...';
-   } else {
-       return data;
-   }
-});
